33329f26924d873c5ee213de32f99a8fbc117a88
socram03 <marcosjgs03@gmail.com>
2022-07-02 11:47:05 -0400

    fix: remove old functions

--- structures/components/SelectMenuOption.ts
+++ structures/components/SelectMenuOption.ts
@@ -6,40 +6,15 @@ import { ComponentEmoji, DiscordSelectOption } from "../../vendor/external.ts";
  */
 export class SelectMenuOption {
     constructor(data: DiscordSelectOption) {
-        this.label = data!.label;
-        this.value = data!.value;
-        this.description = data?.description;
-        this.emoji = data?.emoji;
-        this.default = !!data!.default;
+        this.label = data.label;
+        this.value = data.value;
+        this.description = data.description;
+        this.emoji = data.emoji;
+        this.default = !!data.default;
     }
     label: string;
     value: string;
     description?: string;
     emoji?: ComponentEmoji;
     default?: boolean;
-
-    setLabel(label: string) {
-        this.label = label;
-        return this;
-    }
-
-    setValue(value: string) {
-        this.value = value;
-        return this;
-    }
-
-    setDescription(description: string) {
-        this.description = description;
-        return this;
-    }
-
-    setDefault(Default = true) {
-        this.default = Default;
-        return this;
-    }
-
-    setEmoji(emoji: ComponentEmoji) {
-        this.emoji = emoji;
-        return this;
-    }
 }
