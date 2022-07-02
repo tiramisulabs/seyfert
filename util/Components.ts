beedfe80a6efc86be292d57a7d23575147bf3843
socram03 <marcosjgs03@gmail.com>
2022-07-01 23:07:28 -0400

    fix: createBuilder

--- util/Components.ts
+++ util/Components.ts
@@ -6,11 +6,11 @@ import {
 } from "../vendor/external.ts";
 import { BaseComponent } from "../structures/components/BaseComponent.ts";
 import { ComponentBuilder } from "../structures/builders/ComponentBuilder.ts";
-import { ButtonBuilder, ButtonComponent, InputTextComponent, SelectMenuBuilder, SelectMenuComponent } from "../mod.ts";
+import { ButtonBuilder, ButtonComponent, InputTextComponent, SelectMenuBuilder, SelectMenuComponent, InputTextBuilder } from "../mod.ts";
 
 export type AnyDiscordComponent = DiscordButtonComponent | DiscordInputTextComponent | DiscordSelectMenuComponent;
 export type AnyComponent = ButtonComponent | InputTextComponent | SelectMenuComponent;
-export type AnyComponentBuilder = ButtonBuilder | SelectMenuBuilder;
+export type AnyComponentBuilder = ButtonBuilder | SelectMenuBuilder | InputTextBuilder;
 
 export function createComponent(data: AnyDiscordComponent) {
     if (data instanceof BaseComponent) {
@@ -32,5 +32,16 @@ export function createComponent(data: AnyDiscordComponent) {
 export function createBuilderComponent(data: AnyComponentBuilder | AnyDiscordComponent) {
     if (data instanceof ComponentBuilder) {
         return data;
+    }
+
+    switch (data.type) {
+        case MessageComponentTypes.Button:
+            return new ButtonBuilder(data);
+        case MessageComponentTypes.InputText:
+            return new InputTextBuilder(data);
+        case MessageComponentTypes.SelectMenu:
+            return new SelectMenuBuilder(data);
+        default:
+            throw new Error(`Unknown component type: ${data["type"]}`);
     }
 }
