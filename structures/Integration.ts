cb20e1f04dee76d4937d315fcafc76c235541819
Nicolás Serna <nicolito128@hotmail.com>
2022-07-04 21:02:50 -0300

    Fix Integration account property

--- structures/Integration.ts
+++ structures/Integration.ts
@@ -50,6 +50,6 @@ export class Integration implements Model {
     revoked?: boolean;
 
     user?: DiscordUser;
-    account?: DiscordIntegrationAccount;
+    account: DiscordIntegrationAccount;
     application?: DiscordIntegrationApplication;
 }
