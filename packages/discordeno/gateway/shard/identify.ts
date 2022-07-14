38cf6d79f613b13dd3243dad0fb2a97c8b62137b
Yuzu <yuzuru@programmer.net>
2022-07-14 06:24:16 -0500

    fix(gateway): debug events that seems to trigger in bun

--- packages/discordeno/gateway/shard/identify.ts
+++ packages/discordeno/gateway/shard/identify.ts
@@ -5,7 +5,7 @@ export async function identify(shard: Shard): Promise<void> {
     // A new identify has been requested even though there is already a connection open.
     // Therefore we need to close the old connection and heartbeating before creating a new one.
     if (shard.state === ShardState.Connected) {
-        console.log("CLOSING EXISTING SHARD: #" + shard.id);
+        // console.log("CLOSING EXISTING SHARD: #" + shard.id);
         shard.close(ShardSocketCloseCodes.ReIdentifying, "Re-identifying closure of old connection.");
     }
 
