097a2c65c32f4f400c2abbe1cc6d4deb10ecf20e
Nicolás Serna <nicolito128@hotmail.com>
2022-07-19 03:41:35 -0300

    Update CategoryChannel

--- packages/biscuit/structures/channels.ts
+++ packages/biscuit/structures/channels.ts
@@ -12,7 +12,6 @@ import {
     DiscordInviteMetadata,
     DiscordListArchivedThreads,
     DiscordMessage,
-    DiscordOverwrite,
     DiscordThreadMember,
     DiscordWebhook,
     GatewayOpcodes,
@@ -31,6 +30,7 @@ import Invite from './Invite.ts';
 import Webhook from './Webhook.ts';
 import User from './User.ts';
 import ThreadMember from './ThreadMember.ts';
+import Permissions, { PermissionResolvable } from "./Permissions.ts";
 
 /**
  * Abstract class that represents the base for creating a new channel.
@@ -90,25 +90,41 @@ export abstract class BaseChannel implements Model {
     }
 }
 
-/** CategoryChannel */
+/** 
+ * Represents a category channel.
+ */
 export class CategoryChannel extends BaseChannel {
     constructor(session: Session, data: DiscordChannel) {
         super(session, data);
         this.id = data.id;
         this.name = data.name ? data.name : '';
-        this.permissionOverwrites = data.permission_overwrites ? data.permission_overwrites : undefined;
         this.nsfw = data.nsfw ? data.nsfw : false;
         this.guildId = data.guild_id ? data.guild_id : undefined;
         this.type = ChannelTypes.GuildCategory;
-        this.position = data.position ? data.position : 0;
+        this.position = data.position ? data.position : undefined;
+        this.parentId = data.parent_id ? data.parent_id : undefined;
+
+        this.permissionOverwrites = [] as PermissionsOverwrites[];
+        // TODO: improve this and test
+        if (data.permission_overwrites && data.permission_overwrites.length > 0) {
+            data.permission_overwrites.forEach(v => {
+                this.permissionOverwrites.push({
+                    id: v.id,
+                    type: v.type,
+                    allow: new Permissions(v.allow as PermissionResolvable),
+                    deny: new Permissions(v.deny as PermissionResolvable),
+                } as PermissionsOverwrites);
+            })
+        }
     }
 
     id: Snowflake;
+    parentId?: string;
     name: string;
-    permissionOverwrites?: DiscordOverwrite[];
+    permissionOverwrites: PermissionsOverwrites[];
     nsfw: boolean;
     guildId?: Snowflake;
-    position: number;
+    position?: number;
 }
 
 /** TextChannel */
