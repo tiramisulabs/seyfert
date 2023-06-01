929f480308b0457c4ad8380516e21a77d06bc5ca
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2023-06-01 18:44:25 -0400

    fix

---
+++ packages/core/src/utils/types.ts
@@ -0,0 +1,18 @@
+import type { ImageFormat } from '@biscuitland/common';
+
+export type EditNickname = { nick?: string; reason?: string };
+
+/**
+ * @link https://discord.com/developers/docs/reference#image-formatting
+ */
+export type ImageSize = 16 | 32 | 64 | 128 | 256 | 512 | 1024 | 2048 | 4096;
+export type ImageOptions = {
+  format?: ImageFormat;
+  size?: ImageSize;
+};
+
+export enum ThreadTypes {
+  AnnouncementThread = 10,
+  PublicThread = 11,
+  PrivateThread = 12
+}
