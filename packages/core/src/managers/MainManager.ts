5c00e3b729f4a2844e1080c7f3bbaa8c2723adab
NejireSupremacy <JosueChan2006@outlook.com>
2023-06-04 21:30:45 -0600

    Add: Missing managers on Main

--- packages/core/src/managers/MainManager.ts
+++ packages/core/src/managers/MainManager.ts
@@ -1,8 +1,11 @@
 import type { Session } from '../session';
+import { ApplicationManager } from './ApplicationManager';
 import { ChannelManager } from './ChannelManager';
 import { GuildManager } from './GuildManager';
+import { InteractionManager } from './InteractionManager';
 import { MemberManager } from './MemberManager';
 import { UserManager } from './UserManager';
+import { WebhookManager } from './WebhookManager';
 
 export class MainManager {
   constructor(private readonly session: Session) {
@@ -10,10 +13,16 @@ export class MainManager {
     this.guilds = new GuildManager(this.session);
     this.members = new MemberManager(this.session);
     this.channels = new ChannelManager(this.session);
+    this.application = new ApplicationManager(this.session);
+    this.interaction = new InteractionManager(this.session);
+    this.webhook = new WebhookManager(this.session);
   }
 
   users: UserManager;
   guilds: GuildManager;
   members: MemberManager;
   channels: ChannelManager;
+  application: ApplicationManager;
+  interaction: InteractionManager;
+  webhook: WebhookManager;
 }
