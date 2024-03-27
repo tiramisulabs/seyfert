776c604b3b4230365942eb8145a4fa07ccb2d440
Marcos Susaña <marcosjgs03@gmail.com>
2024-03-27 19:46:28 -0400

    feat: Component Context (#166)
    * feat: component context

--- src/components/command.ts
+++ src/components/command.ts
@@ -1,5 +1,6 @@
-import type { ComponentType } from 'discord-api-types/v10';
-import type { ComponentInteraction, ModalSubmitInteraction, StringSelectMenuInteraction } from '../structures';
+import { ComponentType } from 'discord-api-types/v10';
+import type { ModalSubmitInteraction } from '../structures';
+import type { ComponentCommandInteractionMap, ComponentContext } from './componentcontext';
 
 export const InteractionCommandType = {
 	COMPONENT: 0,
@@ -12,9 +13,14 @@ export interface ComponentCommand {
 
 export abstract class ComponentCommand {
 	type = InteractionCommandType.COMPONENT;
-	abstract componentType: ComponentType;
-	abstract filter(interaction: ComponentInteraction | StringSelectMenuInteraction): Promise<boolean> | boolean;
-	abstract run(interaction: ComponentInteraction | StringSelectMenuInteraction): any;
+	abstract componentType: keyof ComponentCommandInteractionMap;
+	abstract filter(interaction: ComponentContext<typeof this.componentType>): Promise<boolean> | boolean;
+	abstract run(interaction: ComponentContext<typeof this.componentType>): any;
+
+	get cType(): number {
+		// @ts-expect-error
+		return ComponentType[this.componentType];
+	}
 }
 
 export interface ModalCommand {
