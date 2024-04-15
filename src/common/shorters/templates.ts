38ef5d91cf9ff5094ec7719646c7a3b9475128a1
Marcos Susaña <marcosjgs03@gmail.com>
2024-04-15 14:48:44 -0400

    fix: some shorters doesn't return structs (#183)

--- src/common/shorters/templates.ts
+++ src/common/shorters/templates.ts
@@ -1,28 +1,50 @@
 import type { RESTPatchAPIGuildTemplateJSONBody, RESTPostAPIGuildTemplatesJSONBody } from 'discord-api-types/v10';
 import { BaseShorter } from './base';
+import { GuildTemplate } from '../..';
 
 export class TemplateShorter extends BaseShorter {
 	fetch(code: string) {
-		return this.client.proxy.guilds.templates(code).get();
+		return this.client.proxy.guilds
+			.templates(code)
+			.get()
+			.then(template => new GuildTemplate(this.client, template));
 	}
 
 	list(guildId: string) {
-		return this.client.proxy.guilds(guildId).templates.get();
+		return this.client.proxy
+			.guilds(guildId)
+			.templates.get()
+			.then(list => list.map(template => new GuildTemplate(this.client, template)));
 	}
 
 	create(guildId: string, body: RESTPostAPIGuildTemplatesJSONBody) {
-		return this.client.proxy.guilds(guildId).templates.post({ body });
+		return this.client.proxy
+			.guilds(guildId)
+			.templates.post({ body })
+			.then(template => new GuildTemplate(this.client, template));
 	}
 
 	sync(guildId: string, code: string) {
-		return this.client.proxy.guilds(guildId).templates(code).put({});
+		return this.client.proxy
+			.guilds(guildId)
+			.templates(code)
+			.put({})
+			.then(template => new GuildTemplate(this.client, template));
 	}
 
 	edit(guildId: string, code: string, body: RESTPatchAPIGuildTemplateJSONBody) {
-		return this.client.proxy.guilds(guildId).templates(code).patch({ body });
+		return this.client.proxy
+			.guilds(guildId)
+			.templates(code)
+			.patch({ body })
+			.then(template => new GuildTemplate(this.client, template));
 	}
 
 	delete(guildId: string, code: string) {
-		return this.client.proxy.guilds(guildId).templates(code).delete();
+		return this.client.proxy
+			.guilds(guildId)
+			.templates(code)
+			.delete()
+			.then(template => new GuildTemplate(this.client, template));
 	}
 }
