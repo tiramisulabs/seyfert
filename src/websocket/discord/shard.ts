a8380bf07d00e3e55d5c864884099b27e608ac76
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2024-04-24 19:32:28 -0400

    fix: use replaceAll instead

--- src/websocket/discord/shard.ts
+++ src/websocket/discord/shard.ts
@@ -103,7 +103,7 @@ export class Shard {
 				message.d,
 				(_, value) => {
 					if (typeof value === 'string')
-						return value.replace(this.options.token, v => {
+						return value.replaceAll(this.options.token, v => {
 							const split = v.split('.');
 							return `${split[0]}.${'*'.repeat(split[1].length)}.${'*'.repeat(split[2].length)}`;
 						});
