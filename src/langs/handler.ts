4df7b7d7152fd3c21c9668852537539c7a355f3a
MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>
2024-05-06 19:42:53 -0400

    fix: handlers

--- src/langs/handler.ts
+++ src/langs/handler.ts
@@ -1,5 +1,5 @@
 import type { Locale, LocaleString } from 'discord-api-types/v10';
-import { BaseHandler, isCloudfareWorker } from '../common';
+import { BaseHandler } from '../common';
 import { LangRouter } from './router';
 
 export class LangsHandler extends BaseHandler {
@@ -36,12 +36,8 @@ export class LangsHandler extends BaseHandler {
 		return LangRouter(locale, this.defaultLang ?? locale, this.values)();
 	}
 
-	async load(dir: string) {
-		if (isCloudfareWorker()) {
-			console.log('TODO langs');
-			return;
-		}
-		const files = await this.loadFilesK<Record<string, any>>(await this.getFiles(dir));
+	async load(dir: string, instances?: { name: string; file: Record<string, any> }[]) {
+		const files = instances ?? (await this.loadFilesK<Record<string, any>>(await this.getFiles(dir)));
 		for (const i of files) {
 			const locale = i.name.split('.').slice(0, -1).join('.');
 			const result = this.callback(locale, i.file);
