4a08468f89d579d71dac5a3e3bf5c9c6d8f1001b
NotAditya69 <90441096+NotAditya69@users.noreply.github.com>
2024-05-24 20:39:27 +0530

    fix(User#avatarURL): dynamic response, if avatar is a gif return gif

--- src/structures/User.ts
+++ src/structures/User.ts
@@ -37,7 +37,11 @@ export class User extends DiscordBase<APIUser> {
 		if (!this.avatar) {
 			return this.rest.cdn.embed.avatars.get(calculateUserDefaultAvatarIndex(this.id, this.discriminator));
 		}
-		return this.rest.cdn.avatars(this.id).get(this.avatar, options);
+
+		return this.rest.cdn.avatars(this.id).get(this.avatar, {
+			...options,
+			extension: this.avatar.startsWith('a_') ? 'gif' : options?.extension ?? 'webp',
+		});
 	}
 
 	avatarDecorationURL(options?: ImageOptions) {
