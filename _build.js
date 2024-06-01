c523ded8e97c971204c69d7988f3794fd02929e4
MARCROCK22 <marcos22dev@gmail.com>
2024-06-01 19:01:34 +0000

    fix: _build script

--- _build.js
+++ _build.js
@@ -1,9 +1,25 @@
 const { execSync } = require("child_process");
 
-try {
-	execSync('tsc');
-	console.log('[Seyfert]: Builded');
-} catch (e) {
-	console.error(e);
-	console.log('[Seyfert]: Builded with errors');
+const commands = [
+	'tsc',
+	'bunx tsc',
+	'npx tsc',
+	'pnpx tsc'
+]
+
+function executeCommands() {
+	for (const cmd of commands) {
+		try {
+			console.log('[Seyfert]:', 'trying to build with `', cmd, '`')
+			execSync(cmd)
+		} catch (e) {
+			if (cmd === commands.at(-1)) {
+				return console.log('[Seyfert]:', e)
+			}
+		}
+	}
+
+	console.log('[Seyfert]:', 'builded')
 }
+
+executeCommands()
