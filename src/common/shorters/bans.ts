61aa69b2bea36feaf82bab46b55c834f64a8e6d9
NotAditya69 <90441096+NotAditya69@users.noreply.github.com>
2024-06-10 13:27:41 +0530

    fix(BanShorter#bulkCreate): fix method return, bulk-bans returns stats

--- src/common/shorters/bans.ts
+++ src/common/shorters/bans.ts
@@ -15,7 +15,7 @@ export class BanShorter extends BaseShorter {
 	 * @param reason The reason for bulk banning members.
 	 */
 	async bulkCreate(guildId: string, body: RESTPostAPIGuildBulkBanJSONBody, reason?: string) {
-		await this.client.proxy.guilds(guildId)['bulk-bans'].post({ reason, body });
+		return await this.client.proxy.guilds(guildId)['bulk-bans'].post({ reason, body });
 	}
 
 	/**
