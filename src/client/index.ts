42038d0fe9b7cc1d78740718a1cdbe820bef0ffa
Aarón Rafael <69669283+Chewawi@users.noreply.github.com>
2024-06-19 17:52:22 -0300

    chore: export transformers

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,3 +2,4 @@ export type { RuntimeConfig, RuntimeConfigHTTP } from './base';
 export * from './client';
 export * from './httpclient';
 export * from './workerclient';
+export * from './transformers';
