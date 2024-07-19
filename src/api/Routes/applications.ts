969d47372b57fe23eb5e69e8f1de1f60f5cf2854
Tony Supremacy <165050835+VanStk@users.noreply.github.com>
2024-07-19 14:21:45 +0000

    feat: add support for new Discord application emojis features

--- src/api/Routes/applications.ts
+++ src/api/Routes/applications.ts
@@ -23,6 +23,9 @@ import type {
 	RESTPutAPIApplicationRoleConnectionMetadataJSONBody,
 	RESTPutAPIApplicationRoleConnectionMetadataResult,
 	RESTPutAPIGuildApplicationCommandsPermissionsResult,
+	APIEmoji,
+	RESTPostAPIGuildEmojiJSONBody,
+	RESTPatchAPIGuildEmojiJSONBody,
 } from 'discord-api-types/v10';
 import type { ProxyRequestMethod } from '../Router';
 import type { RestArguments } from '../api';
@@ -93,5 +96,51 @@ export interface ApplicationRoutes {
 				): Promise<RESTPutAPIApplicationRoleConnectionMetadataResult>;
 			};
 		};
+		emojis: {
+			get(
+				args?: RestArguments<ProxyRequestMethod.Get, RESTGetAPIApplicationEmojisQuery>,
+			): Promise<RESTGetAPIApplicationEmojiResult>;
+			post(
+				args?: RestArguments<ProxyRequestMethod.Post, RESTPostAPIApplicationEmojiJSONBody>,
+			): Promise<RESTPostAPIApplicationEmojiJSONBody>;
+			patch(
+				args?: RestArguments<ProxyRequestMethod.Patch, RESTPatchAPIApplicationEmojiJSONBody>,
+			): Promise<RESTPatchAPIApplicationEmojiJSONBody>;
+			delete(args?: RestArguments<ProxyRequestMethod.Delete, RESTGetAPIApplicationEmojisQuery>): Promise<never>;
+		};
 	};
 }
+
+/**
+ * https://discord.com/developers/docs/resources/emoji#list-application-emojis
+ */
+export interface RESTGetAPIApplicationEmojisResult {
+	items: APIEmoji[];
+}
+
+/**
+ * https://discord.com/developers/docs/resources/emoji#get-application-emoji
+ */
+export type RESTGetAPIApplicationEmojiResult = APIEmoji;
+
+/**
+ * https://discord.com/developers/docs/resources/emoji#create-application-emoji-json-params
+ */
+export type RESTPostAPIApplicationEmojiJSONBody = Pick<RESTPostAPIGuildEmojiJSONBody, 'image' | 'name'>;
+
+/**
+ * https://discord.com/developers/docs/resources/emoji#create-application-emoji
+ */
+export type RESTPostAPIApplicationEmojiResult = APIEmoji;
+
+/**
+ * https://discord.com/developers/docs/resources/emoji#modify-application-emoji
+ */
+export type RESTPatchAPIApplicationEmojiJSONBody = Pick<RESTPatchAPIGuildEmojiJSONBody, 'name'>;
+
+/**
+ * https://discord.com/developers/docs/resources/emoji#modify-application-emoji
+ */
+export type RESTPatchAPIApplicationEmojiResult = APIEmoji;
+
+export type RESTGetAPIApplicationEmojisQuery = Pick<RESTGetAPIApplicationEmojiResult, 'id'>;
