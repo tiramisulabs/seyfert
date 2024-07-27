97cd39eb10219c3a0c373e26e2fb665a99e26abc
Andrés <andrewdotdev@gmail.com>
2024-07-27 18:21:38 +0200

    fix(Base): data is optional (#225)

--- src/builders/Base.ts
+++ src/builders/Base.ts
@@ -3,7 +3,7 @@ import type { APIBaseComponent, ComponentType } from '../types';
 export abstract class BaseComponentBuilder<
 	TYPE extends Partial<APIBaseComponent<ComponentType>> = APIBaseComponent<ComponentType>,
 > {
-	constructor(public data: Partial<TYPE>) {}
+	constructor(public data: Partial<TYPE> = {}) {}
 
 	toJSON(): TYPE {
 		return { ...this.data } as TYPE;
