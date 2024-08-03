4061f006250d483690e7d162e77b6b64a53bea6d
MARCROCK22 <marcos22dev@gmail.com>
2024-08-03 03:35:30 +0000

    feat: update constants

--- src/api/utils/constants.ts
+++ src/api/utils/constants.ts
@@ -1,7 +1,7 @@
-export const DefaultUserAgent = 'DiscordBot (https://seyfert.dev)';
+export const DefaultUserAgent = 'DiscordBot (https://seyfert.dev, v2.0.0)';
 export const ALLOWED_EXTENSIONS = ['webp', 'png', 'jpg', 'jpeg', 'gif'] as const;
 export const ALLOWED_STICKER_EXTENSIONS = ['png', 'json', 'gif'] as const;
-export const ALLOWED_SIZES = [16, 32, 64, 128, 256, 512, 1_024, 2_048, 4_096] as const;
+export const ALLOWED_SIZES = [16, 32, 64, 100, 128, 256, 512, 1_024, 2_048, 4_096] as const;
 
 export type ImageExtension = (typeof ALLOWED_EXTENSIONS)[number];
 export type StickerExtension = (typeof ALLOWED_STICKER_EXTENSIONS)[number];
