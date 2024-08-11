084c85368e1d11dfced6e246a5417377b3402f5f
Marcos Susaña <marcosjgs03@gmail.com>
2024-08-10 20:13:02 -0400

    feat: reload langs

--- src/langs/handler.ts
+++ src/langs/handler.ts
@@ -1,10 +1,11 @@
-import { BaseHandler } from '../common';
+import { BaseHandler, isCloudfareWorker, magicImport } from '../common';
 import { LangRouter } from './router';
 import type { FileLoaded } from '../commands/handler';
 import type { LocaleString, Locale } from '../types';
 
 export class LangsHandler extends BaseHandler {
 	values: Partial<Record<string, any>> = {};
+	private __paths: Partial<Record<string, string>> = {};
 	protected filter = (path: string) =>
 		path.endsWith('.js') || (!path.endsWith('.d.ts') && path.endsWith('.ts')) || path.endsWith('.json');
 	defaultLang?: string;
@@ -47,12 +48,34 @@ export class LangsHandler extends BaseHandler {
 	parse(file: EventInstance) {
 		const locale = file.name.split('.').slice(0, -1).join('.') || file.name;
 		const result = this.onFile(locale, file.file);
+		if ('path' in file) this.__paths[locale] = file.path as string;
 		if (result) this.values[locale] = result;
 	}
 
 	set(instances: EventInstance[]) {
 		for (const i of instances) {
 			this.parse(i);
+		}
+	}
+
+	async reload(lang: string) {
+		if (isCloudfareWorker()) {
+			throw new Error('Reload in cloudfare worker is not supported');
+		}
+		const value = this.__paths[lang];
+		if (!value) return null;
+		delete require.cache[value];
+
+		return (this.values[lang] = await magicImport(value).then(x => this.onFile(lang, x)));
+	}
+
+	async reloadAll(stopIfFail = true) {
+		for (const i in this.__paths) {
+			try {
+				await this.reload(i);
+			} catch (e) {
+				if (stopIfFail) throw e;
+			}
 		}
 	}
 
