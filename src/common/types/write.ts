29f7b769e174f713ec042bbd4bb0d1bf8a9cfda6
Marcos Susaña <marcosjgs03@gmail.com>
2024-09-04 17:33:24 -0400

    feat: polls improvement (#249)
    * feat: polls improvement
    
    * fix: poll in editOrReply
    
    * feat(api-types): type APIEmbedPollResult
    
    ---------
    
    Co-authored-by: MARCROCK22 <57925328+MARCROCK22@users.noreply.github.com>

--- src/common/types/write.ts
+++ src/common/types/write.ts
@@ -54,14 +54,14 @@ export type MessageWebhookCreateBodyRequest = OmitInsert<
 
 export type MessageWebhookUpdateBodyRequest = OmitInsert<
 	RESTPatchAPIWebhookWithTokenMessageJSONBody,
-	'components' | 'embeds',
+	'components' | 'embeds' | 'poll',
 	ResolverProps
 >;
 
 export type InteractionMessageUpdateBodyRequest = OmitInsert<
 	RESTPatchAPIWebhookWithTokenMessageJSONBody,
-	'components' | 'embeds',
-	ResolverProps
+	'components' | 'embeds' | 'poll',
+	SendResolverProps
 >;
 
 export type ComponentInteractionMessageUpdate = OmitInsert<
