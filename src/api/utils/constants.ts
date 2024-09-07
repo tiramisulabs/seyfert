8cc495a4dbd62c02272bf980f033831147f4f29d
Marcos Susaña <marcosjgs03@gmail.com>
2024-09-06 22:23:08 -0400

    chore: release

--- src/api/utils/constants.ts
+++ src/api/utils/constants.ts
@@ -1,4 +1,4 @@
-export const DefaultUserAgent = 'DiscordBot (https://seyfert.dev, v2.0.0)';
+export const DefaultUserAgent = 'DiscordBot (https://seyfert.dev, v2.1.0)';
 export const ALLOWED_EXTENSIONS = ['webp', 'png', 'jpg', 'jpeg', 'gif'] as const;
 export const ALLOWED_STICKER_EXTENSIONS = ['png', 'json', 'gif'] as const;
 export const ALLOWED_SIZES = [16, 32, 64, 100, 128, 256, 512, 1_024, 2_048, 4_096] as const;
