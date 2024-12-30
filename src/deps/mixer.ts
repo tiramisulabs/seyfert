ac2df8133b828b6e57ff5fb6ade151e4200b02e8
Marcos Susaña <marcosjgs03@gmail.com>
2024-12-29 21:21:24 -0400

    fix: return support node18 (#317)

--- src/deps/mixer.ts
+++ src/deps/mixer.ts
@@ -47,7 +47,7 @@ export function Mixin<T, C extends TypeClass[]>(...args: C): C[number] & T {
 	const ignoreOverwriteToString = Object.keys(Object.getOwnPropertyDescriptors(args[0].prototype)).includes('toString');
 	function MixedClass(...constructorArgs: any[]) {
 		for (const i of args) {
-			const descriptors = getDescriptors(i).toReversed();
+			const descriptors = getDescriptors(i).reverse();
 			for (const j of descriptors) {
 				// @ts-expect-error
 				Object.assign(this, new j.constructor.value(...constructorArgs));
