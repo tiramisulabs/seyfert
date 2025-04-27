f4bcaa58ecbf4bf0c60bc1f5b6d529882b564cb0
MARCROCK22 <marcos22dev@gmail.com>
2025-04-27 12:19:30 -0400

    fix: components type

--- src/common/types/write.ts
+++ src/common/types/write.ts
@@ -17,7 +17,7 @@ import type { OmitInsert } from './util';
 
 export interface ResolverProps {
 	embeds?: Embed[] | APIEmbed[] | undefined;
-	components?: TopLevelBuilders[];
+	components?: TopLevelBuilders[] | TopLevelBuilders['toJSON'][];
 	files?: AttachmentBuilder[] | Attachment[] | RawFile[] | undefined;
 }
 
