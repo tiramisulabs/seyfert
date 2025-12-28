3ce44a7b3c72b985fe7bcee162d82405da574e90
MARCROCK22 <marcos22dev@gmail.com>
2025-12-28 12:10:55 -0400

    feat: extend ImageResolvable type to include AttachmentBuilder

--- src/common/types/resolvables.ts
+++ src/common/types/resolvables.ts
@@ -1,4 +1,4 @@
-import type { Attachment, AttachmentDataType, AttachmentResolvable } from '../../builders';
+import type { Attachment, AttachmentBuilder, AttachmentDataType, AttachmentResolvable } from '../../builders';
 import type { GuildMember } from '../../structures';
 import type { APIGuildMember, APIPartialEmoji, RESTPostAPIApplicationEmojiJSONBody } from '../../types';
 import type { EmbedColors, OmitInsert } from '..';
@@ -6,7 +6,7 @@ import type { EmbedColors, OmitInsert } from '..';
 export type EmojiResolvable = string | Partial<APIPartialEmoji> | `<${string | undefined}:${string}:${string}>`;
 export type GuildMemberResolvable = string | Partial<GuildMember> | APIGuildMember;
 export type ColorResolvable = `#${string}` | number | keyof typeof EmbedColors | 'Random' | [number, number, number];
-export type ImageResolvable = { data: AttachmentResolvable; type: AttachmentDataType } | Attachment;
+export type ImageResolvable = { data: AttachmentResolvable; type: AttachmentDataType } | Attachment | AttachmentBuilder;
 export type ApplicationEmojiResolvable = OmitInsert<
 	RESTPostAPIApplicationEmojiJSONBody,
 	'image',
