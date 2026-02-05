a47c94eecb5094594edf36091a7322defa845da6
Socram03 <socram@seyfert.dev>
2026-02-05 08:04:21 -0400

    fix: handle empty keys in set methods

--- src/cache/resources/default/guild-related.ts
+++ src/cache/resources/default/guild-related.ts
@@ -54,6 +54,8 @@ export class GuildRelatedResource<T = any, S = any> {
 			this.filter(x[1], x[0] as string, guild, from),
 		) as [string, any][];
 
+		if (!keys.length) return fakePromise(undefined).then(() => {}) as void;
+
 		return fakePromise(
 			this.addToRelationship(
 				keys.map(x => x[0]),
@@ -75,6 +77,8 @@ export class GuildRelatedResource<T = any, S = any> {
 		const keys = (Array.isArray(__keys) ? __keys : [[__keys, data]]).filter(x =>
 			this.filter(x[1], x[0], guild, from),
 		) as [string, any][];
+
+		if (!keys.length) void fakePromise(undefined);
 
 		return fakePromise(
 			this.addToRelationship(
